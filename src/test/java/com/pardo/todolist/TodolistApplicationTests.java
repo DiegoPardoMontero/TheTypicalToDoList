5ba65822071d5a791e3075387ba83dcd10da2a1c
Diego Pardo Montero <di-pardo@javeriana.edu.co>
2023-12-26 21:41:04 -0500

    Creada estructura inicial del proyecto

---
+++ src/test/java/com/pardo/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pardo.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
