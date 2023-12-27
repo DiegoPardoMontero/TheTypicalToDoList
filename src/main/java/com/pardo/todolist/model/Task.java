244c491b962783057055ca8be2f6fb89d25196eb
Diego Pardo Montero <di-pardo@javeriana.edu.co>
2023-12-26 22:08:06 -0500

    Añadida clase básica Tarea

---
+++ src/main/java/com/pardo/todolist/model/Task.java
@@ -0,0 +1,40 @@
+package com.pardo.todolist.model;
+
+public class Task {
+    private int id;
+    private String title;
+    private String description;
+    private boolean completed;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
